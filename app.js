82e82c77a3f514eb2fffc8107b75c65a5818d241
Dipendra Karki <dippu9119@gmail.com>
2024-08-22 18:14:59 +0545

    start

--- app.js
+++ app.js
@@ -1,78 +1,112 @@
-require('dotenv').config()
-const express = require('express');
-const connectToDatabase = require('./database');
-const Blog = require('./model/blogModel');
-
-const app = express()
-app.use(express.json())
- const {multer,storage} = require('./middleware/multerConfig');
-const upload = multer ({storage : storage})
-connectToDatabase()
-
-app.get("/", (req, res) => {
-    res.req("This is Home page")
-})
-
-app.get("/about", (req, res) => {
-    res.status(200).json({
-        message: "Hello i am Dipendra karki"
-    })
-})
-
-
-
-
-app.post("/blog",upload.single('image'), async (req, res) => {
-    const { title, description, image, subtitle } = req.body
-    if (!title || !description || !subtitle || !image) {
-        res.status(400).json({
-            message: "Enter the following details"
-        })
-    }
-    // console.log(req.body)
-    await Blog.create({
-        title: title,
-        description: description,
-        subtitle: subtitle,
-        image: image
-    })
-    res.status(200).json({
-        message: "Blog api hit successfully"
-    })
-})
-app.listen(process.env.PORT, () => {
-    console.log("Good Night Dipendra")
-})
-
-
-
-
-
 // require('dotenv').config()
-// const express = require('express')
-// const connectToDatabase = require('./database')
+// const express = require('express');
+// const connectToDatabase = require('./database');
+// const Blog = require('./model/blogModel');
 
-// const app = express() 
+// const app = express()
 // app.use(express.json())
-// const {multer,storage} = require('./middleware/multerConfig')
-// const upload = multer({storage : storage })
-
+//  const {multer,storage} = require('./middleware/multerConfig');
+// const upload = multer ({storage : storage})
 // connectToDatabase()
 
-// app.get("/",(req,res)=>{
+// app.get("/", (req, res) => {
+//     res.req("This is Home page")
+// })
+
+// app.get("/about", (req, res) => {
 //     res.status(200).json({
-//         hello : "This is home page"
+//         message: "Hello i am Dipendra karki"
+//     })
+// })
+// app.post("/blog",upload.single('image'), async (req, res) => {
+//     const { title, description, image, subtitle } = req.body
+//     const filename = req.file.filename
+//     console.log(req.file)
+//     if (!title || !description || !subtitle || !image) {
+//          res.status(400).json({
+//             message: "Enter the following details"
+//         })
+//         return
+//     }
+//     console.log(req.file)
+//     await Blog.create({
+//         title: title,
+//         description: description,
+//         subtitle: subtitle,
+//         image: filename
+//     })
+//     res.status(200).json({
+//         message: "Blog api hit successfully"
 //     })
 // })
 
-// app.post("/blog",upload.single('image'), (req,res)=>{
-//     console.log(req.body)
+// app.get("/blog",async(req,res)=>{
+//     const blogs = await Blog.find()
 //     res.status(200).json({
-//         message : "Blog api hit successfully"
+//         message : 'Blogs Fetchs Successfully',
+//         data : blogs
 //     })
 // })
+// app.use(express.static("./storage"))
 
-// app.listen(process.env.PORT,()=>{
-//     console.log("NodeJs project has started")
+// app.listen(process.env.PORT, () => {
+//     console.log("Good Night Dipendra")
 // })
+
+
+require('dotenv').config()
+const express = require('express')
+const connectToDatabase = require('./database')
+
+const app = express() 
+app.use(express.json())
+const {multer,storage} = require('./middleware/multerConfig')
+const Blog = require('./model/blogModel')
+const upload = multer({storage : storage })
+
+connectToDatabase()
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        hello : "This is home page"
+    })
+})
+
+app.post("/blog",upload.single('image'), async (req,res)=>{
+   const {title,subtitle,description} = req.body 
+   const filename = req.file.filename 
+
+   if(!title || !subtitle || !description){
+        return res.status(400).json({
+            message : "Please provide title,subtitle,description"
+        })
+        
+   }
+   await Blog.create({
+    title : title, 
+    subtitle : subtitle, 
+    description : description, 
+    image : filename
+   })
+    
+    res.status(200).json({
+        message : "Blog api hit successfully"
+    })
+})
+
+app.get("/blog",async (req,res)=>{
+   const blogs =  await Blog.find() // returns array
+   res.status(200).json({
+    message : "Blogs fetched successfully", 
+    data : blogs
+   })
+})
+
+app.use(express.static('./storage'))
+
+app.listen(process.env.PORT,()=>{
+    console.log("NodeJs project has started")
+})
+
+
 
--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -1,36 +1,36 @@
-// const multer = require('multer');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination : function(req,file,cb){
+        cb(null,'./storage')
+    },
+    filename : function(req,file,cb){
+        cb(null,"Manish-" + file.originalname)
+    }
+})  
+
+
+module.exports = {
+    multer,
+    storage
+}
+
+
+
+
+
+// const multer = require('multer')
 
 // const storage = multer.diskStorage({
 //     destination : function(req,file,cb){
-//         cb(null,'./storage')
-//     },
+//         cb(null,'./storage') // cb(error,success)
+//     }, 
 //     filename : function(req,file,cb){
 //         cb(null,"Manish-" + file.originalname)
 //     }
-// })  
-
+// })
 
 // module.exports = {
 //     multer,
 //     storage
 // }
-
-
-
-
-
-const multer = require('multer')
-
-const storage = multer.diskStorage({
-    destination : function(req,file,cb){
-        cb(null,'./storage') // cb(error,success)
-    }, 
-    filename : function(req,file,cb){
-        cb(null,"Manish-" + file.originalname)
-    }
-})
-
-module.exports = {
-    multer, 
-    storage
-}
